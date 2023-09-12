1ce8bd97dad9191e213c5a296ddfb5775dc449ec
Tony Julien <tonyuj@microsoft.com>
2023-09-12 16:25:45 -0700

    Added "my voice" navigation and fragment

---
+++ app/src/main/java/com/apps/amplifty/ui/myvoice/MyVoiceViewModel.kt
@@ -0,0 +1,13 @@
+package com.apps.amplifty.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MyVoiceViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is My Voice registration page (Coming soon)"
+    }
+    val text: LiveData<String> = _text
+}
