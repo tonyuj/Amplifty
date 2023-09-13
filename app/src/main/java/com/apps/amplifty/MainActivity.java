47cc183c37773697064205c058870c6154d94d4a
Wendy Wang <wenwang@microsoft.com>
2023-09-12 20:02:06 -0700

    basic functionality for speech to text is working

---
+++ app/src/main/java/com/apps/amplifty/MainActivity.java
@@ -0,0 +1,264 @@
+package com.apps.amplifty;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.microsoft.cognitiveservices.speech.audio.AudioConfig;
+import com.microsoft.cognitiveservices.speech.SpeechConfig;
+import com.microsoft.cognitiveservices.speech.SpeechRecognizer;
+import com.microsoft.cognitiveservices.speech.KeywordRecognitionModel;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static android.Manifest.permission.INTERNET;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.RECORD_AUDIO;
+
+public class MainActivity extends AppCompatActivity {
+
+    //
+    // Configuration for speech recognition
+    //
+
+    // Replace below with your own subscription key
+    private static final String SpeechSubscriptionKey = "";
+    // Replace below with your own service region (e.g., "westus").
+    private static final String SpeechRegion = "eastus";
+    private static final String KwsModelFile = "kws.table";
+
+    private TextView recognizedTextView;
+
+    private Button recognizeButton;
+    private Button recognizeContinuousButton;
+
+    private MicrophoneStream microphoneStream;
+    private MicrophoneStream createMicrophoneStream() {
+        this.releaseMicrophoneStream();
+
+        microphoneStream = new MicrophoneStream();
+        return microphoneStream;
+    }
+    private void releaseMicrophoneStream() {
+        if (microphoneStream != null) {
+            microphoneStream.close();
+            microphoneStream = null;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recognizedTextView = findViewById(R.id.recognizedText);
+        recognizedTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        recognizeButton = findViewById(R.id.buttonRecognize);
+        recognizeContinuousButton = findViewById(R.id.buttonRecognizeContinuous);
+
+        // Initialize SpeechSDK and request required permissions.
+        try {
+            // a unique number within the application to allow
+            // correlating permission request responses with the request.
+            int permissionRequestId = 5;
+
+            // Request permissions needed for speech recognition
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{RECORD_AUDIO, INTERNET, READ_EXTERNAL_STORAGE}, permissionRequestId);
+        }
+        catch(Exception ex) {
+            Log.e("SpeechSDK", "could not init sdk, " + ex.toString());
+            recognizedTextView.setText("Could not initialize: " + ex.toString());
+        }
+
+        // create config
+        final SpeechConfig speechConfig;
+        final KeywordRecognitionModel kwsModel;
+        try {
+            speechConfig = SpeechConfig.fromSubscription(SpeechSubscriptionKey, SpeechRegion);
+            kwsModel = KeywordRecognitionModel.fromFile(copyAssetToCacheAndGetFilePath(KwsModelFile));
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            displayException(ex);
+            return;
+        }
+
+        ///////////////////////////////////////////////////
+        // recognize continuously
+        ///////////////////////////////////////////////////
+        recognizeContinuousButton.setOnClickListener(new View.OnClickListener() {
+            private static final String logTag = "reco 3";
+            private boolean continuousListeningStarted = false;
+            private SpeechRecognizer reco = null;
+            private AudioConfig audioInput = null;
+            private String buttonText = "";
+            private ArrayList<String> content = new ArrayList<>();
+
+            @Override
+            public void onClick(final View view) {
+                final Button clickedButton = (Button) view;
+                disableButtons();
+                if (continuousListeningStarted) {
+                    if (reco != null) {
+                        final Future<Void> task = reco.stopContinuousRecognitionAsync();
+                        setOnTaskCompletedListener(task, result -> {
+                            Log.i(logTag, "Continuous recognition stopped.");
+                            MainActivity.this.runOnUiThread(() -> {
+                                clickedButton.setText(buttonText);
+                            });
+                            enableButtons();
+                            continuousListeningStarted = false;
+                        });
+                    } else {
+                        continuousListeningStarted = false;
+                    }
+
+                    return;
+                }
+
+                clearTextBox();
+
+                try {
+                    content.clear();
+
+                    audioInput = AudioConfig.fromStreamInput(createMicrophoneStream());
+                    reco = new SpeechRecognizer(speechConfig, audioInput);
+
+                    reco.recognizing.addEventListener((o, speechRecognitionResultEventArgs) -> {
+                        final String s = speechRecognitionResultEventArgs.getResult().getText();
+                        Log.i(logTag, "Intermediate result received: " + s);
+                        content.add(s);
+                        setRecognizedText(TextUtils.join(" ", content));
+                        content.remove(content.size() - 1);
+                    });
+
+                    reco.recognized.addEventListener((o, speechRecognitionResultEventArgs) -> {
+                        final String s = speechRecognitionResultEventArgs.getResult().getText();
+                        Log.i(logTag, "Final result received: " + s);
+                        content.add(s);
+                        setRecognizedText(TextUtils.join(" ", content));
+                    });
+
+                    final Future<Void> task = reco.startContinuousRecognitionAsync();
+                    setOnTaskCompletedListener(task, result -> {
+                        continuousListeningStarted = true;
+                        MainActivity.this.runOnUiThread(() -> {
+                            buttonText = clickedButton.getText().toString();
+                            clickedButton.setText("Stop");
+                            clickedButton.setEnabled(true);
+                        });
+                    });
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                    displayException(ex);
+                }
+            }
+        });
+    }
+
+
+    private void displayException(Exception ex) {
+        recognizedTextView.setText(ex.getMessage() + System.lineSeparator() + TextUtils.join(System.lineSeparator(), ex.getStackTrace()));
+    }
+
+    private void clearTextBox() {
+        AppendTextLine("", true);
+    }
+
+    private void setRecognizedText(final String s) {
+        AppendTextLine(s, true);
+    }
+
+    private void AppendTextLine(final String s, final Boolean erase) {
+        MainActivity.this.runOnUiThread(() -> {
+            if (erase) {
+                recognizedTextView.setText(s);
+            } else {
+                String txt = recognizedTextView.getText().toString();
+                recognizedTextView.setText(txt + System.lineSeparator() + s);
+            }
+        });
+    }
+
+    private void disableButtons() {
+        MainActivity.this.runOnUiThread(() -> {
+            recognizeButton.setEnabled(false);
+            recognizeContinuousButton.setEnabled(false);
+        });
+    }
+
+    private void enableButtons() {
+        MainActivity.this.runOnUiThread(() -> {
+            recognizeButton.setEnabled(true);
+            recognizeContinuousButton.setEnabled(true);
+        });
+    }
+
+    private <T> void setOnTaskCompletedListener(Future<T> task, OnTaskCompletedListener<T> listener) {
+        s_executorService.submit(() -> {
+            T result = task.get();
+            listener.onCompleted(result);
+            return null;
+        });
+    }
+
+    private interface OnTaskCompletedListener<T> {
+        void onCompleted(T taskResult);
+    }
+
+    private String copyAssetToCacheAndGetFilePath(String filename) {
+        File cacheFile = new File(getCacheDir() + "/" + filename);
+        if (!cacheFile.exists()) {
+            try {
+                InputStream is = getAssets().open(filename);
+                int size = is.available();
+                byte[] buffer = new byte[size];
+                is.read(buffer);
+                is.close();
+                FileOutputStream fos = new FileOutputStream(cacheFile);
+                fos.write(buffer);
+                fos.close();
+            }
+            catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return cacheFile.getPath();
+    }
+
+    private static ExecutorService s_executorService;
+    static {
+        s_executorService = Executors.newCachedThreadPool();
+    }
+
+    public class Word {
+        public String word;
+        public String errorType;
+        public double accuracyScore;
+        public long duration;
+        public long offset;
+        public Word(String word, String errorType) {
+            this.word = word;
+            this.errorType = errorType;
+        }
+
+        public Word(String word, String errorType, double accuracyScore, long duration, long offset) {
+            this(word, errorType);
+            this.accuracyScore = accuracyScore;
+            this.duration = duration;
+            this.offset = offset;
+        }
+    }
+}
